dc1cec3e0bb08f03d02c80a201b2c9bb125ba1c6
Rafaell Estevam <rafa-estevam@hotmail.com>
2023-01-29 19:41:26 -0300

    criando o api gateway para roteamento das requisições

---
+++ ms-api-gateway-zuul/src/main/java/com/spring/hrapigatewayzuul/MsApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package com.spring.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class MsApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayZuulApplication.class, args);
+	}
+
+}
