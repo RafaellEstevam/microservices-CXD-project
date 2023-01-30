5dd4ab5d6aa222a8542250c3ee254fe3bd847a6c
Rafaell Estevam <rafa-estevam@hotmail.com>
2023-01-30 04:52:57 -0300

    configurando o gateway para atuar como Resource server

--- ms-api-gateway-zuul/src/main/java/com/spring/msapigatewayzuul/config/AppConfig.java
+++ ms-api-gateway-zuul/src/main/java/com/spring/msapigatewayzuul/config/AppConfig.java
@@ -1,27 +1,22 @@
-package com.spring.msoauth.config;
+package com.spring.msapigatewayzuul.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
 
+
     @Value("${oauth.signature}")
     private String signature;
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-        accessTokenConverter.setSigningKey(signature);
+        accessTokenConverter.setSigningKey("MY-SECRET-KEY");
         return accessTokenConverter;
     }
 
